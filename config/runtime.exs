a821886fef7790b81e39d75dadc475c820ceceaa
femi <cocofemi@gmail.com>
2025-05-03 21:19:47 +0100

    changes

--- config/runtime.exs
+++ config/runtime.exs
@@ -60,7 +60,7 @@ if config_env() == :prod do
     check_origin: [~r/.*/],
     http: [
       port: String.to_integer(System.get_env("PORT") || "4000"),
-      transport_options: [socket_opts: [:inet6]]
+      #transport_options: [socket_opts: [:inet6]]
     ],
     secret_key_base: System.fetch_env!("SECRET_KEY_BASE"),
     server: true
