b799c9d873e1bbcb2424640f06d11dda05450a62
femi <cocofemi@gmail.com>
2025-05-03 21:25:10 +0100

    changes

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,27 +1,8 @@
 import Config
 
-# config/runtime.exs is executed for all environments, including
-# during releases. It is executed after compilation and before the
-# system starts, so it is typically used to load production configuration
-# and secrets from environment variables or elsewhere. Do not define
-# any compile-time configuration in here, as it won't be applied.
-# The block below contains prod specific runtime configuration.
-
-# ## Using releases
-#
-# If you use `mix release`, you need to explicitly enable the server
-# by passing the PHX_SERVER=true when you start it:
-#
-#     PHX_SERVER=true bin/markdown_editor start
-#
-# Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
-# script that automatically sets the env var above.
-if System.get_env("PHX_SERVER") do
-  config :markdown_editor, MarkdownEditorWeb.Endpoint, server: true
-end
-
 if config_env() == :prod do
   IO.puts(">>> Runtime loaded. Setting up endpoint...")
+
   database_url =
     System.fetch_env!("DATABASE_URL") ||
       raise """
@@ -29,20 +10,6 @@ if config_env() == :prod do
       For example: ecto://USER:PASS@HOST/DATABASE
       """
 
-  maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
-
-   config :markdown_editor, MarkdownEditor.Repo,
-    url: database_url,
-    ssl: true,
-    ssl_opts: [verify: :verify_none],
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: [:inet6]
-
-  # The secret key base is used to sign/encrypt cookies and other secrets.
-  # A default value is used in config/dev.exs and config/test.exs but you
-  # want to use a different value for prod and you most likely don't want
-  # to check this value into version control, so we use an environment
-  # variable instead.
   secret_key_base =
     System.get_env("SECRET_KEY_BASE") ||
       raise """
@@ -50,79 +17,21 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("PHX_HOST") || "markdownchallenge.onrender.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
-  config :markdown_editor, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
+  config :markdown_editor, MarkdownEditor.Repo,
+    url: database_url,
+    ssl: true,
+    ssl_opts: [verify: :verify_none],
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
   config :markdown_editor, MarkdownEditorWeb.Endpoint,
-    url: [host: "markdownchallenge.onrender.com", port: 443],
-    check_origin: [~r/.*/],
-    http: [
-      port: String.to_integer(System.get_env("PORT") || "4000"),
-      #transport_options: [socket_opts: [:inet6]]
-    ],
-    secret_key_base: System.fetch_env!("SECRET_KEY_BASE"),
+    url: [host: host, port: 443],
+    check_origin: [~r/.*/], # temporarily allow all origins
+    http: [port: port],
+    secret_key_base: secret_key_base,
     server: true
-    # http: [
-    #   # Enable IPv6 and bind on all interfaces.
-    #   # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-    #   # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
-    #   # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-    #   ip: {0, 0, 0, 0, 0, 0, 0, 0},
-    #   port: port
-    # ],
 
-
-  # ## SSL Support
-  #
-  # To get SSL working, you will need to add the `https` key
-  # to your endpoint configuration:
-  #
-  #     config :markdown_editor, MarkdownEditorWeb.Endpoint,
-  #       https: [
-  #         ...,
-  #         port: 443,
-  #         cipher_suite: :strong,
-  #         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-  #         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-  #       ]
-  #
-  # The `cipher_suite` is set to `:strong` to support only the
-  # latest and more secure SSL ciphers. This means old browsers
-  # and clients may not be supported. You can set it to
-  # `:compatible` for wider support.
-  #
-  # `:keyfile` and `:certfile` expect an absolute path to the key
-  # and cert in disk or a relative path inside priv, for example
-  # "priv/ssl/server.key". For all supported SSL configuration
-  # options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
-  #
-  # We also recommend setting `force_ssl` in your config/prod.exs,
-  # ensuring no data is ever sent via http, always redirecting to https:
-  #
-  #     config :markdown_editor, MarkdownEditorWeb.Endpoint,
-  #       force_ssl: [hsts: true]
-  #
-  # Check `Plug.SSL` for all available options in `force_ssl`.
-
-  # ## Configuring the mailer
-  #
-  # In production you need to configure the mailer to use a different adapter.
-  # Also, you may need to configure the Swoosh API client of your choice if you
-  # are not using SMTP. Here is an example of the configuration:
-  #
-  #     config :markdown_editor, MarkdownEditor.Mailer,
-  #       adapter: Swoosh.Adapters.Mailgun,
-  #       api_key: System.get_env("MAILGUN_API_KEY"),
-  #       domain: System.get_env("MAILGUN_DOMAIN")
-  #
-  # For this example you need include a HTTP client required by Swoosh API client.
-  # Swoosh supports Hackney and Finch out of the box:
-  #
-  #     config :swoosh, :api_client, Swoosh.ApiClient.Hackney
-  #
-  # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
-
-
+  config :markdown_editor, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 end
