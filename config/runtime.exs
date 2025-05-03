1fa9092a176b0127fc7ebe0f6175663885284b35
femi <cocofemi@gmail.com>
2025-05-03 21:08:25 +0100

    changes

--- config/runtime.exs
+++ config/runtime.exs
@@ -57,11 +57,7 @@ if config_env() == :prod do
 
   config :markdown_editor, MarkdownEditorWeb.Endpoint,
     url: [host: "markdownchallenge.onrender.com", port: 443],
-    check_origin: [
-  "https://markdownchallenge.onrender.com",
-  ~r/^https?:\/\/(www\.)?markdownchallenge\.onrender\.com$/
-],
-
+    check_origin: [~r/.*/],
     http: [
       port: String.to_integer(System.get_env("PORT") || "4000"),
       transport_options: [socket_opts: [:inet6]]
