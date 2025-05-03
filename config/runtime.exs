5e67dae3225a27f250d2eed880515157d4bf3c47
femi <cocofemi@gmail.com>
2025-05-03 21:43:30 +0100

    changes

--- config/runtime.exs
+++ config/runtime.exs
@@ -3,21 +3,8 @@ import Config
 if config_env() == :prod do
   IO.puts(">>> Runtime loaded. Setting up endpoint...")
 
-  database_url =
-    System.fetch_env!("DATABASE_URL") ||
-      raise """
-      environment variable DATABASE_URL is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-      """
-
-  secret_key_base =
-    System.get_env("SECRET_KEY_BASE") ||
-      raise """
-      environment variable SECRET_KEY_BASE is missing.
-      You can generate one by calling: mix phx.gen.secret
-      """
-
-  host = System.get_env("PHX_HOST") || "markdownchallenge.onrender.com"
+  database_url = System.fetch_env!("DATABASE_URL")
+  secret_key_base = System.fetch_env!("SECRET_KEY_BASE")
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :markdown_editor, MarkdownEditor.Repo,
@@ -27,11 +14,12 @@ if config_env() == :prod do
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
   config :markdown_editor, MarkdownEditorWeb.Endpoint,
-    url: [host: host, port: 443],
-    check_origin: [~r/.*/], # temporarily allow all origins
+    url: [host: "markdownchallenge.onrender.com", port: 443],
     http: [port: port],
+    check_origin: [
+      "https://markdownchallenge.onrender.com",
+      ~r/^https?:\/\/(www\.)?markdownchallenge\.onrender\.com$/
+    ],
     secret_key_base: secret_key_base,
     server: true
-
-  config :markdown_editor, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 end
